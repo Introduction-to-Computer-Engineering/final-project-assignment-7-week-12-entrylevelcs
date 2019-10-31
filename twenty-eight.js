f67fd23cd959442c4f2f45b154effae695d0dd5d
entrylevelcs <54680246+entrylevelcs@users.noreply.github.com>
2019-10-31 15:16:32 -0600

    Update twenty-eight.js

--- twenty-eight.js
+++ twenty-eight.js
@@ -17,7 +17,7 @@ function snake() {
         }
         yCoord++
     }
-    //the following if statements change which LED is enabled based on yCoord value
+    //the following three if statements change which LED is enabled based on yCoord value
     if (yCoord >= 0 && yCoord <= 1) {
         pins.analogWritePin(AnalogPin.P8, 0)
         pins.analogWritePin(AnalogPin.P12, 1023)
