df354a69e272eef0ede51e96d5cc4951fc3711eb
entrylevelcs <54680246+entrylevelcs@users.noreply.github.com>
2019-10-31 15:16:16 -0600

    Update twenty-eight.js

--- twenty-eight.js
+++ twenty-eight.js
@@ -3,6 +3,7 @@ let yCoord: number = 0
 pins.analogWritePin(AnalogPin.P8, 0)
 pins.analogWritePin(AnalogPin.P12, 0)
 pins.analogWritePin(AnalogPin.P16, 0)
+//starts the LEDs at an off state
 basic.forever(function () {
     snake()
 })
@@ -16,6 +17,7 @@ function snake() {
         }
         yCoord++
     }
+    //the following if statements change which LED is enabled based on yCoord value
     if (yCoord >= 0 && yCoord <= 1) {
         pins.analogWritePin(AnalogPin.P8, 0)
         pins.analogWritePin(AnalogPin.P12, 1023)
